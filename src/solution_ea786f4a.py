979f0d86ec4dec9c57d48e320a7886d2c3c343f2
lauragonzalezb <lauragonzalezb@users.noreply.github.com>
2019-11-24 23:04:52 +0000

    changed author names

--- src/solution_ea786f4a.py
+++ src/solution_ea786f4a.py
@@ -2,6 +2,12 @@
 """
 Created on Sun Nov 24 22:35:30 2019
 
-@author: laura 
+@author: Laura Gonzalez, Praneeth Jakkaraju, Tudor Chiribes
 """
+import numpy as np
 
+def solve(t):
+  arr_shape = np.array(t)
+  arr_shape = arr.shape
+  #print(arr_shape)
+  
