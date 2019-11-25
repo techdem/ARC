a5c76671ac5b04855510c9a0ca4326ea90b360f7
Praneeth Jakkaraju <jakkaraju.praneeth@fleetmatics.com>
2019-11-25 15:26:21 +0000

    adding print method to print the output and code to run the python file as module

--- src/solution_9565186b.py
+++ src/solution_9565186b.py
@@ -1,4 +1,37 @@
-import numpy as np 
+import numpy as np
+import sys
+import json
+
+
+def print_solution(json_input):
+  """
+  print the training grids and compute the solution for each one
+  """
+
+  for l in json_input['train']:
+    for row in l['input']:
+      print(row)
+    print("")
+    solution = solve(l['input'])
+    if solution == l['output']:
+      for row in solution:
+        print(row)
+    else:
+      print("Solution is wrong!")
+    print("")
+
+  for l in json_input['test']:
+    for row in l['input']:
+      print(row)
+    print("")
+    solution = solve(l['input'])
+    if solution == l['output']:
+      for row in solution:
+        print(row)
+    else:
+      print("Solution is wrong!")
+    print("")
+
 
 def solve(input):
   input_array = np.array(input)
@@ -9,4 +42,21 @@ def solve(input):
     for j,lst_element in enumerate(input_lst):
       if (lst_element != b):
         input_lst[j] = 5
-  print(input_array)
+  return input_array
+
+
+if __name__ == "__main__":
+  # read path to JSON file from command line arguments
+  file_path = str(sys.argv[1])
+
+  # open the file containing the data
+  input_file = open(file_path)
+
+  # relative path for development
+  # input_file = open('../data/training/9565186b.json')
+
+  # load the input as a JSON array
+  json_input = json.load(input_file)
+
+  # visualise the task demonstration
+  print_solution(json_input)
