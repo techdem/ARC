5d153b4e6372684cfd32da0b7420f112358dd9b1
Praneeth Jakkaraju <jakkaraju.praneeth@fleetmatics.com>
2019-11-25 16:07:07 +0000

    updating names of variables used in 9565186b and adding report document v1

--- src/solution_9565186b.py
+++ src/solution_9565186b.py
@@ -1,3 +1,9 @@
+"""
+Created on Sun Nov 24 22:35:30 2019
+
+@author: Laura Gonzalez, Praneeth Jakkaraju, Tudor Chiribes
+"""
+
 import numpy as np
 import sys
 import json
@@ -37,10 +43,10 @@ def solve(input):
   input_array = np.array(input)
   unique_code,count = np.unique(input_array, return_counts=True)
   code_count = dict(zip(unique_code,count))
-  b = max(code_count, key = code_count.get)
+  max_occured_code = max(code_count, key = code_count.get)
   for input_lst in input_array:
     for j,lst_element in enumerate(input_lst):
-      if (lst_element != b):
+      if (lst_element != max_occured_code):
         input_lst[j] = 5
   return input_array
 
