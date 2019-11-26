b36797379498aabaa21b4f91bb8fcec1a02af151
lauragonzalezb <lauragonzalezb@users.noreply.github.com>
2019-11-26 20:37:54 +0000

    implementation for algo

--- src/solution_ea786f4a.py
+++ src/solution_ea786f4a.py
@@ -7,7 +7,9 @@ Created on Sun Nov 24 22:35:30 2019
 import numpy as np
 
 def solve(t):
-  arr_shape = np.array(t)
-  arr_shape = arr.shape
-  #print(arr_shape)
-  
+    arr = np.array(t)
+    midvalue = int(len(arr.diagonal()) / 2 )
+    filler = arr.diagonal()[midvalue]
+    np.fill_diagonal(arr,filler)
+    np.fill_diagonal(np.fliplr(arr),filler)
+    print(arr)
