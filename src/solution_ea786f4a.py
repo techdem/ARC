dd18e07b4ada91682947f8ba9cb509e2a3e95537
lauragonzalezb <lauragonzalezb@users.noreply.github.com>
2019-11-26 20:56:57 +0000

    explaning the algo

--- src/solution_ea786f4a.py
+++ src/solution_ea786f4a.py
@@ -7,9 +7,9 @@ Created on Sun Nov 24 22:35:30 2019
 import numpy as np
 
 def solve(t):
-    input_array = np.array(t)
-    midvalue = int(len(input_array.diagonal()) / 2 )
-    fill_with_color = input_array.diagonal()[midvalue]
-    np.fill_diagonal(input_array,fill_with_color)
-    np.fill_diagonal(np.fliplr(input_array),fill_with_color)
-    return input_array.tolist()
+    input_array = np.array(t) # converting the input to array 
+    midvalue = int(len(input_array.diagonal()) / 2 ) # calculating the middle value
+    fill_with_color = input_array.diagonal()[midvalue] # select the color 
+    np.fill_diagonal(input_array,fill_with_color) # filling with selected color 
+    np.fill_diagonal(np.fliplr(input_array),fill_with_color) 
+    return input_array.tolist() # convert back to a list of lists
