5f58b855f7f07b0fa7d397b51704e4e0bea220d6
lauragonzalezb <lauragonzalezb@users.noreply.github.com>
2019-11-26 20:51:03 +0000

    renaming variables

--- src/solution_ea786f4a.py
+++ src/solution_ea786f4a.py
@@ -7,9 +7,9 @@ Created on Sun Nov 24 22:35:30 2019
 import numpy as np
 
 def solve(t):
-    arr = np.array(t)
-    midvalue = int(len(arr.diagonal()) / 2 )
-    filler = arr.diagonal()[midvalue]
-    np.fill_diagonal(arr,filler)
-    np.fill_diagonal(np.fliplr(arr),filler)
-    print(arr)
+    input_array = np.array(t)
+    midvalue = int(len(input_array.diagonal()) / 2 )
+    fill_with_color = input_array.diagonal()[midvalue]
+    np.fill_diagonal(input_array,fill_with_color)
+    np.fill_diagonal(np.fliplr(input_array),fill_with_color)
+    return input_array.tolist()
