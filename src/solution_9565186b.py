50f83db715395a69b18669505137aa99d6375251
Praneeth Jakkaraju <jakkaraju.praneeth@fleetmatics.com>
2019-11-26 21:00:27 +0000

    Adding comments to code

--- src/solution_9565186b.py
+++ src/solution_9565186b.py
@@ -10,59 +10,66 @@ import json
 
 
 def print_solution(json_input):
-  """
-  print the training grids and compute the solution for each one
-  """
+    """
+    print the training grids and compute the solution for each one
+    """
 
-  for l in json_input['train']:
-    for row in l['input']:
-      print(row)
-    print("")
-    solution = solve(l['input'])
-    if solution == l['output']:
-      for row in solution:
-        print(row)
-    else:
-      print("Solution is wrong!")
-    print("")
+    for l in json_input['train']:
+        for row in l['input']:
+            print(row)
+        print("")
+        solution = solve(l['input'])
+        if solution == l['output']:
+            for row in solution:
+                print(row)
+        else:
+            print("Solution is wrong!")
+        print("")
 
-  for l in json_input['test']:
-    for row in l['input']:
-      print(row)
-    print("")
-    solution = solve(l['input'])
-    if solution == l['output']:
-      for row in solution:
-        print(row)
-    else:
-      print("Solution is wrong!")
-    print("")
+    for l in json_input['test']:
+        for row in l['input']:
+            print(row)
+        print("")
+        solution = solve(l['input'])
+        if solution == l['output']:
+            for row in solution:
+                print(row)
+        else:
+            print("Solution is wrong!")
+        print("")
 
 
 def solve(input):
-  input_array = np.array(input)
-  unique_code,count = np.unique(input_array, return_counts=True)
-  code_count = dict(zip(unique_code,count))
-  max_occured_code = max(code_count, key = code_count.get)
-  for input_lst in input_array:
-    for j,lst_element in enumerate(input_lst):
-      if (lst_element != max_occured_code):
-        input_lst[j] = 5
-  return input_array.tolist()
+    # convert the input to numpy array
+    input_array = np.array(input)
+    # get the count of cells with color code
+    unique_code,count = np.unique(input_array, return_counts=True)
+    # making a dictionary of color code and count of cells with color
+    code_count = dict(zip(unique_code,count))
+    # get the maximum count of cells colored same
+    max_occured_code = max(code_count, key = code_count.get)
+    # enumerate through input array
+    for input_lst in input_array:
+        for j,lst_element in enumerate(input_lst):
+            """check if each cell is not colored the selected color code
+            and replace it with color code 5"""
+            if (lst_element != max_occured_code):
+                input_lst[j] = 5
+    return input_array.tolist()
 
 
 if __name__ == "__main__":
-  # read path to JSON file from command line arguments
-  file_path = str(sys.argv[1])
+    # read path to JSON file from command line arguments
+    file_path = str(sys.argv[1])
 
-  # open the file containing the data
-  input_file = open(file_path)
+    # open the file containing the data
+    input_file = open(file_path)
 
-  # relative path for development
-  # input_file = open('../data/training/9565186b.json')
+    # relative path for development
+    # input_file = open('../data/training/9565186b.json')
 
-  # load the input as a JSON array
-  json_input = json.load(input_file)
+    # load the input as a JSON array
+    json_input = json.load(input_file)
 
-  # visualise the task demonstration
-  print_solution(json_input)
+    # visualise the task demonstration
+    print_solution(json_input)
