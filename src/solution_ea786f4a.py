829181d7e4fe121b177c5a59c65878fa94188664
Praneeth Jakkaraju <jakkaraju.praneeth@fleetmatics.com>
2019-11-26 22:23:10 +0000

    Adding main method and print to solution

--- src/solution_ea786f4a.py
+++ src/solution_ea786f4a.py
@@ -5,6 +5,37 @@ Created on Sun Nov 24 22:35:30 2019
 @author: Laura Gonzalez, Praneeth Jakkaraju, Tudor Chiribes
 """
 import numpy as np
+import json
+import sys
+
+def print_solution(json_input):
+    """
+    print the training grids and compute the solution for each one
+    """
+
+    for l in json_input['train']:
+        for row in l['input']:
+            print(row)
+        print("")
+        solution = solve(l['input'])
+        if solution == l['output']:
+            for row in solution:
+                print(row)
+        else:
+            print("Solution is wrong!")
+        print("")
+
+    for l in json_input['test']:
+        for row in l['input']:
+            print(row)
+        print("")
+        solution = solve(l['input'])
+        if solution == l['output']:
+            for row in solution:
+                print(row)
+        else:
+            print("Solution is wrong!")
+        print("")
 
 def solve(t):
     input_array = np.array(t) # converting the input to array 
@@ -13,3 +44,20 @@ def solve(t):
     np.fill_diagonal(input_array,fill_with_color) # filling with selected color 
     np.fill_diagonal(np.fliplr(input_array),fill_with_color) 
     return input_array.tolist() # convert back to a list of lists
+
+
+if __name__ == "__main__":
+    # read path to JSON file from command line arguments
+    file_path = str(sys.argv[1])
+
+    # open the file containing the data
+    input_file = open(file_path)
+
+    # relative path for development
+    # input_file = open('../data/training/ea786f4a.json')
+
+    # load the input as a JSON array
+    json_input = json.load(input_file)
+
+    # visualise the task demonstration
+    print_solution(json_input)
