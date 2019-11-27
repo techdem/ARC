fbaf709566cac90feed025df902f0cf2c9a70351
techdem <tudor.chiribes@gmail.com>
2019-11-27 12:17:45 +0000

    adding clarification for print function

--- src/solution_28bf18c6.py
+++ src/solution_28bf18c6.py
@@ -15,18 +15,23 @@ def print_solution(json_input):
     print the training grids and compute the solution for each one
     """
     
+    # iterate over the contents of 'train'
     for l in json_input['train']:
+        # each list represent a row of the grid
         for row in l['input']:
             print(row)
         print("")
+        # compute the output using the solve method
         solution = solve(l['input'])
+        # if the result of solve() matches the original output, print
         if solution == l['output']:
             for row in solution:
                 print(row)
         else:
             print("Solution is wrong!")
         print("")
-            
+    
+    # repeat above process for the contents of 'test'
     for l in json_input['test']:
         for row in l['input']:
             print(row)
