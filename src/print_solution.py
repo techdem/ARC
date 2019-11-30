33fbadf4ad3e9180c4e816ad339b0224672a0cca
techdem <tudor.chiribes@gmail.com>
2019-11-30 18:49:14 +0000

    updated function to unpack and print output only

--- src/print_solution.py
+++ src/print_solution.py
@@ -5,29 +5,22 @@ def print_solution(json_input, solve):
     
     # iterate over the contents of 'train'
     for l in json_input['train']:
-        # each list represent a row of the grid
-        for row in l['input']:
-            print(row)
-        print("")
         # compute the output using the solve method
         solution = solve(l['input'])
         # if the result of solve() matches the original output, print
         if solution == l['output']:
             for row in solution:
-                print(row)
+                print(*row)
         else:
             print("Solution is wrong!")
         print("")
     
     # repeat above process for the contents of 'test'
     for l in json_input['test']:
-        for row in l['input']:
-            print(row)
-        print("")
         solution = solve(l['input'])
         if solution == l['output']:
             for row in solution:
-                print(row)
+                print(*row)
         else:
             print("Solution is wrong!")
         print("")
